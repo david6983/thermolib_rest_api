21e782557b4bd86b32099d06be76dc07a025ebf8
david <dav6983@gmail.com>
2019-12-17 14:05:14 +0100

    initial commit

---
+++ src/main/java/com/thermistor/api/RestApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.thermistor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thermistor/api/RestApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thermistor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
