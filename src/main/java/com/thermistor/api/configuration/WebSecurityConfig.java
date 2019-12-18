0ac49f54a5dd3a25a6fce59bc039f8c045963454
david <dav6983@gmail.com>
2019-12-18 11:34:30 +0100

    Fix swagger-ui without authenticate

--- src/main/java/com/thermistor/api/configuration/WebSecurityConfig.java
+++ src/main/java/com/thermistor/api/configuration/WebSecurityConfig.java
@@ -54,7 +54,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                .authorizeRequests().antMatchers(
+             "/authenticate",
+                        "/v2/api-docs",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**"
+                ).permitAll().
                 // all other requests need to be authenticated
                         anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
